6ba730775318cbe670ae78bc648bf6bff15fe6fb
Clare Vivas Lucas <clarevivaslucas@Clares-MacBook-Pro.local>
2014-05-06 08:49:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwesomeWebsite::Application.config.secret_key_base = '2fb5278f7a834fc700c9077ad1e9f3111fec50db13eede19587e49692f1aa3262ff1ee95f4d6f27d380c51b9de80e0983f8553b97b902722af939466b38e5449'
